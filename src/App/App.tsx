45367a5d5837b425841ab71dddd9191033cc3e98
MarceloLudovino <marcelo.docutec@gmail.com>
2019-10-12 15:22:58 -0300

    first commit

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Movies TS
+      </header>
+    </div>
+  );
+}
+
+export default App;
