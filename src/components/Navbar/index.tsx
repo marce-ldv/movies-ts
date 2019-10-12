61a8d972e6d59d3fe35688a080856f44b6141277
MarceloLudovino <marcelo.docutec@gmail.com>
2019-10-12 16:42:17 -0300

    Add grids in Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { AppBar, Typography } from '@material-ui/core';
+import { AppBar, Typography, Container } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 
 export const Navbar: React.FunctionComponent = () => {
   return (
     <AppBar position="static">
-      <Grid container justify="center" component="div">
-        <Grid item xs={3} component="div">
-          <Typography variant="h6">Movies TS</Typography>
+      <Container fixed>
+        <Grid container justify="center" component="div">
+          <Grid item xs={6} component="div">
+            <Typography variant="h6">Movies TS</Typography>
+          </Grid>
+          <Grid item xs={6} component="div">
+            <Typography variant="h6">Categories</Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={3} component="div">
-          <Typography variant="h6">Categories</Typography>
-        </Grid>
-      </Grid>
+      </Container>
     </AppBar>
   );
 };
