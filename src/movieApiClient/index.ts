15e722204a5e0188bb4d8da612ab727ed4b0aa11
sanson222 <molinasergio91@gmail.com>
2019-10-13 20:47:24 -0300

    add entry point to movie service

--- src/movieApiClient/index.ts
+++ src/movieApiClient/index.ts
@@ -1,12 +1,27 @@
 import axios, { AxiosResponse } from 'axios';
 
-export type ENDPOINT = 'trending';
-
 const instance = axios.create({
   baseURL: `https://api.themoviedb.org/3/${process.env.API_KEY}`,
   method: 'get',
 });
 
-export const movieDbService = <T>(endpoint: ENDPOINT): Promise<AxiosResponse<T>> => {
+const movieDbServiceBase = <T>(endpoint: string): Promise<AxiosResponse<T>> => {
   return instance.get<T>(endpoint);
+};
+
+/**
+ * Trending
+ */
+export type mediaTypes = 'all' | 'movie' | 'tv' | 'person';
+export type timeWindows = 'day' | 'week';
+
+export type trendingParams = {
+  mediaTypes: mediaTypes;
+  timeWindows: timeWindows;
+};
+
+export const movieDbService = {
+  trending<T>({ mediaTypes, timeWindows }: trendingParams) {
+    return movieDbServiceBase<T>(`/trending/${mediaTypes}/${timeWindows}`);
+  },
 };
