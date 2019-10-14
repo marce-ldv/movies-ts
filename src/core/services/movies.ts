4c75f152bda4db3b420683a09253832cb1e263d0
sanson222 <molinasergio91@gmail.com>
2019-10-13 23:00:15 -0300

    some fixes

--- src/core/services/movies.ts
+++ src/core/services/movies.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
 
+//TODO: legacy Code
 export const getMovies = async <T>() =>
   await axios.get<T>(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}`);
